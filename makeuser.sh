f445969ca9f1b3fef2381174a0b5254adc49bee3
darthbeaner <beanpole109@gmail.com>
2016-10-14 23:35:11 -0400

    Update makeuser.sh

--- makeuser.sh
+++ makeuser.sh
@@ -1,34 +1,11 @@
 #!/bin/bash
-# So $1 is username, $2 is first, $3 is group.
-# This will create the following files:
-#       Personal
-#       Care Plan
-#       
-# Then create the following directories:
-#       Progress Notes
-#       Medical Records
-#
-#######################################################
+################################################
+# So $1 is username, $2 is first, $3 is group. #
+################################################
 
 adduser --disabled-password $1 --gecos $2,,,,
 usermod -g $3 $1
-touch /home/$1/Personal
-setfacl -m g:Manager:rwx /home/$1/Personal
-setfacl -m g:Patient:r /home/$1/Personal
-setfacl -m g:Health_Care_Provider:rwx /home/$1/Personal
-setfacl -m g:Doctor:rwx /home/$1/Personal
-touch /home/$1/'Care Plan'
-setfacl -m g:Manager:rwx /home/$1/'Care Plan'
-setfacl -m g:Patient:r /home/$1/'Care Plan'
-setfacl -m g:Health_Care_Provider:rwx /home/$1/'Care Plan'
-setfacl -m g:Doctor:rwx /home/$1/'Care Plan'
-mkdir /home/$1/'Progress Notes'
-setfacl -m g:Manager:rwx /home/$1/'Progress Notes'
-setfacl -m g:Patient:r /home/$1/'Progress Notes'
-setfacl -m g:Health_Care_Provider:rwx /home/$1/'Progress Notes'
-setfacl -m g:Doctor:rwx /home/$1/'Progress Notes'
-mkdir /home/$1/'Medical Records'
-setfacl -m g:Manager:rwx /home/$1/'Medical Records'
-setfacl -m g:Patient:r /home/$1/'Medical Records'
-setfacl -m g:Health_Care_Provider:rwx /home/$1/'Medical Records'
-setfacl -m g:Doctor:rwx /home/$1/'Medical Records'
+touch /home/Personal/$1_PersonalData       
+touch /home/'Care Plan'/$1_CarePlan
+touch /home/'Medical Records'/$1_MedicalNotes
+touch /home/'Progress Notes'/$1_ProgressNote1
